7fe48e1fdf951e775d6348bf1e64fb6e6162cd9e
Oscar Ssentoogo <ossentoogo@nephilaadvisors.co.uk>
2018-06-06 21:44:43 +0100

    latest

--- src/Learn/Core/Views/Login/LoginView.xaml.cs
+++ src/Learn/Core/Views/Login/LoginView.xaml.cs
@@ -1,12 +1,23 @@
-﻿using Learn.Core.Views.Base;
+﻿using System.Reactive.Disposables;
+using Learn.Core.Views.Base;
+using ReactiveUI;
 
 namespace Learn.Core.Views.Login
 {
 	public partial class LoginView : ContentPageBase<LoginViewModel>
 	{
-		public LoginView()
+
+	    protected readonly CompositeDisposable SubscriptionDisposables = new CompositeDisposable();
+        public LoginView()
 		{
 			InitializeComponent ();
-		}
-	}
+
+		    this.WhenActivated(disposables =>
+		    {
+		        this.OneWayBind(ViewModel, x => x.UserName, x => x.Email.Text).DisposeWith(SubscriptionDisposables);
+		        this.OneWayBind(ViewModel, x => x.Password, x => x.Password.Text, x => x).DisposeWith(SubscriptionDisposables);
+		    });
+
+        }
+    }
 }
